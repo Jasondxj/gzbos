7c43fa77ce233ac1cef68a00652d8706235a71d5
Jason <291417099@qq.com>
2019-05-04 16:09:40 +0800

    整合Activity

---
+++ src/com/jason/bos/web/action/ProcessDefinitionAction.java
@@ -0,0 +1,9 @@
+package com.jason.bos.web.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class ProcessDefinitionAction extends ActionSupport{
+    public String list(){
+        return "list";
+    }
+}
